5712c55b3455dcbe41a2b4806dcbb386f9d34893
harlock184 <105575194+harlock184@users.noreply.github.com>
2025-01-07 02:43:07 -0600

    facebook_ui parte 3, clase de circle_button para reutilizar el codigo

--- lib/facebook_ui/facebook_ui.dart
+++ lib/facebook_ui/facebook_ui.dart
@@ -2,8 +2,7 @@ import 'package:facebook_ui/assets/icons/custom_icons_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
-
+import 'widgets/circle_button.dart';
 
 class FacebookUi extends StatelessWidget {
   @override
@@ -12,76 +11,45 @@ class FacebookUi extends StatelessWidget {
       appBar: AppBar(
         systemOverlayStyle: const SystemUiOverlayStyle(
           statusBarIconBrightness: Brightness.dark,
-        ) ,
+        ),
         backgroundColor: Colors.transparent,
         leading: SvgPicture.asset("lib/assets/logos/facebook.svg",
-        colorFilter: const ColorFilter.mode(
-          Colors.blueAccent, 
-          BlendMode.srcIn,)
-        ),
+            colorFilter: const ColorFilter.mode(
+              Colors.blueAccent,
+              BlendMode.srcIn,
+            )),
         leadingWidth: 150,
-        
-        actions: [
-          Container(
-            width: 40,
-            height: 40,
-            decoration: const BoxDecoration(
-              color: Colors.grey,
-              shape: BoxShape.circle,
-            ),
-            child:const Icon(
-              CustomIcons.search,
-            color: Colors.white,
-            size: 16,
-            ),
+        actions: const [
+          CircleButton(
+            color: Color(0xffBFBFBF),
+            iconData: CustomIcons.search,
           ),
-
-           Container(
-            width: 40,
-            height: 40,
-            decoration: const BoxDecoration(
-              color: Colors.redAccent,
-              shape: BoxShape.circle,
-            ),
-            child:const Icon(
-              CustomIcons.bell,
-            color: Colors.white,
-            size: 16,
-            ),
+          SizedBox(
+            width: 15,
           ),
-
-           Container(
-            width: 40,
-            height: 40,
-            decoration: const BoxDecoration(
-              color: Colors.grey,
-              shape: BoxShape.circle,
-            ),
-            child:const Icon(
-              CustomIcons.user_friends,
-            color: Colors.white,
-            size: 16,
-            ),
+          CircleButton(
+            color: Color(0xffFE7574),
+            iconData: CustomIcons.bell,
           ),
-
-           Container(
-            width: 40,
-            height: 40,
-            decoration: const BoxDecoration(
-              color: Colors.grey,
-              shape: BoxShape.circle,
-            ),
-            child:const Icon(
-              CustomIcons.messenger,
-            color: Colors.white,
-            size: 16,
-            ),
+          SizedBox(
+            width: 15,
+          ),
+          CircleButton(
+            color: Color(0xff7BBAFF),
+            iconData: CustomIcons.user_friends,
+          ),
+          SizedBox(
+            width: 15,
+          ),
+          CircleButton(
+            color: Color(0xff1C86E4),
+            iconData: CustomIcons.messenger,
+          ),
+          SizedBox(
+            width: 15,
           ),
         ],
-
-      
       ),
     );
   }
-
 }
